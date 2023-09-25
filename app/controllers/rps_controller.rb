014a6a68e0c85b67231a8ad78043b5cb1be55bb4
Ethan Huang <96758695+EthanKH@users.noreply.github.com>
2023-09-25 21:51:37 +0000

    finished rps on rails

---
+++ app/controllers/rps_controller.rb
@@ -0,0 +1,41 @@
+class RpsController < ApplicationController
+  def rock_you
+    @random = ["rock", "paper", "scissors"].sample
+    if @random == "rock"
+      @outcome = "tied"
+    elsif @random == "paper"
+      @outcome = "lost"
+    elsif @random == "scissors"
+      @outcome = "won"
+    end
+    render ({ :template => "game_templates/play_rock" })
+  end
+
+  def paper_you
+    @random = ["rock", "paper", "scissors"].sample
+    if @random == "paper"
+      @outcome = "tied"
+    elsif @random == "scissors"
+      @outcome = "lost"
+    elsif @random == "rock"
+      @outcome = "won"
+    end
+    render ({ :template => "game_templates/play_paper" })
+  end
+
+  def scissors_you
+    @random = ["rock", "paper", "scissors"].sample
+    if @random == "scissors"
+      @outcome = "tied"
+    elsif @random == "rock"
+      @outcome = "lost"
+    elsif @random == "paper"
+      @outcome = "won"
+    end
+    render ({ :template => "game_templates/play_scissors" })
+  end
+
+  def rule 
+    render ({ :template => "game_templates/rules" })
+  end
+end  
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-
+  get("/rock", { :controller => "rps", :action => "rock_you" })
+  #DO NOT PUT SPACES BETWEEN GET AND (), SHOULD LOOK LIKE
+  #get()
+  get("/paper", { :controller => "rps", :action => "paper_you" })
+  get("/scissors", { :controller => "rps", :action => "scissors_you" })
+  get("/", { :controller => "rps", :action => "rule" })
 end
